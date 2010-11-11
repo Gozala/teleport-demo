ce594bc37ffa5235a0b53b7b80b3db0758c87c69
Irakli Gozalishvili <rfobic@gmail.com>
2010-11-11 16:45:08 +0100

    Bundling.

---
+++ packages/helo-world/app.js
@@ -0,0 +1,8 @@
+define("helo-world/app", ["mustache"], function(require, exports, module, undefined) { var mustache = require('mustache')
+exports.main = function main() {
+  document.body.innerHTML = mustache.to_html('Helo {{name}}', { name: 'world' })
+}
+// Executing main function if module loaded as a program.
+if (module == require.main) exports.main()
+ 
+/**/});
